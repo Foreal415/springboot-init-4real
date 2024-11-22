daf204957a2bd82f00c361eb47cf717bfaa6cca1
4real <1692787232@qq.com>
2024-11-22 16:50:43 +0800

    后端模板1.00

---
+++ src/main/java/com/foreal/springbootinit/controller/PostThumbController.java
@@ -0,0 +1,53 @@
+package com.foreal.springbootinit.controller;
+
+import com.foreal.springbootinit.common.BaseResponse;
+import com.foreal.springbootinit.common.ErrorCode;
+import com.foreal.springbootinit.common.ResultUtils;
+import com.foreal.springbootinit.exception.BusinessException;
+import com.foreal.springbootinit.model.dto.postthumb.PostThumbAddRequest;
+import com.foreal.springbootinit.model.entity.User;
+import com.foreal.springbootinit.service.PostThumbService;
+import com.foreal.springbootinit.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
